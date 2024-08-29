8e843e55b5bde25b0dc243f5f3c9d9ee146bbf78
Seungwoo-Yoon <dotori000@snu.ac.kr>
2024-08-29 17:21:45 +0900

    phi debug 12

--- models/Phi.py
+++ models/Phi.py
@@ -17,7 +17,7 @@ class Phi(ModelInterface):
 
         self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'  # TODO: fix this
         self.processor = AutoProcessor.from_pretrained(model, trust_remote_code=True)
-        self.model = AutoModelForCausalLM.from_pretrained(model, torch_dtype=torch.bfloat16, low_cpu_mem_usage=True, trust_remote_code=True).to(self.device)
+        self.model = AutoModelForCausalLM.from_pretrained(model, torch_dtype='auto', low_cpu_mem_usage=True, trust_remote_code=True, _attn_implementation='flash_attention_2').to(self.device)
 
     def run(self, image_path, prompt):
         conversation = [
@@ -33,7 +33,7 @@ class Phi(ModelInterface):
         inputs = self.processor(prompt, [image], return_tensors='pt').to(self.device)
 
         output = self.model.generate(**inputs, max_new_tokens=self.max_tokens, eos_token_id=self.processor.tokenizer.eos_token_id, temperature=self.temperature, do_sample=True if self.temperature > 0 else False, top_p=None)[0]
-        output = self.processor.decode(output, skip_special_tokens=True)[0]
+        output = self.processor.decode(output, skip_special_tokens=True)
 
         output = output.replace(prompt, '')
 
