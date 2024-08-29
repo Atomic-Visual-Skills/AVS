49dee1587abc479289e5845e4f89133c7e15093d
Seungwoo-Yoon <dotori000@snu.ac.kr>
2024-08-29 16:33:26 +0900

    Phi debug 1

--- models/Phi.py
+++ models/Phi.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils import *
 
 
-class InternVL(ModelInterface):
+class Phi(ModelInterface):
     def __init__(self, model='microsoft/Phi-3.5-vision-instruct', temperature=0, max_tokens=1024):
         self.temperature = temperature
         self.max_tokens = max_tokens
