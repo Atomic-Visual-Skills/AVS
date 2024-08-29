e8b8fe4f065031955a97ec4727b504d0bfeaca72
Seungwoo-Yoon <dotori000@snu.ac.kr>
2024-08-29 16:49:20 +0900

    phi debug 7

--- models/Phi.py
+++ models/Phi.py
@@ -28,12 +28,11 @@ class Phi(ModelInterface):
         ]
 
         prompt = self.processor.tokenizer.apply_chat_template(conversation, tokenize=False, add_generation_prompt=True)
-        print(prompt)
 
         image = open_image(image_path)
         inputs = self.processor(prompt, [image], return_tensors='pt').to(self.device)
 
-        output = self.model.generate(**inputs, max_new_tokens=self.max_tokens, temperature=self.temperature, do_sample=True, top_p=None)[0]
+        output = self.model.generate(**inputs, max_new_tokens=self.max_tokens, temperature=self.temperature, do_sample=True if self.temperature > 0 else False, top_p=None)[0]
         output = self.processor.decode(output, skip_special_tokens=False)
 
         output = output.replace(prompt, '')
